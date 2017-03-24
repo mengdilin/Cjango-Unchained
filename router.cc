2af9e5a8f3575e348d41a93d6c4a50f2b95ea487
caprice-j <tnk.yasutaka@gmail.com>
2017-03-23 22:53:02 -0400

    implement add_route() in part (working)

--- router.cc
+++ router.cc
@@ -1,5 +1,18 @@
-#include <stdio.h>
+#include "router.hh"
+
+void Router::add_route(std::string url_pattern, functor f) {
+  patterns_list.push_back(url_pattern);
+  printf("add a new rule: %s\n", url_pattern.c_str());
+}
+
+HttpResponse callback_1 (HttpRequest) {
+  HttpResponse a = HttpResponse();
+  return a;
+}
+
 int main(){
-  printf("hello cpp\n");
+  URLmap m;
+  m["/abc"] = callback_1;
+  Router r(m);
   return 0;
 }
--- router.hh
+++ router.hh
@@ -1,3 +1,38 @@
+#include <stdio.h>
+#include <functional>
+#include <map>
+#include <vector>
+#include <string>
+
+// FIXME mock
+class HttpRequest {
+public:
+  int some_data_member;
+  HttpRequest() : some_data_member(3) {};
+};
+class HttpResponse {
+public:
+  int some_data_member;
+  HttpResponse() : some_data_member(3) {};
+};
+
+typedef std::function<HttpResponse(HttpRequest)> functor;
+typedef std::map<std::string, functor> URLmap;
+
+
 class Router {
-  void add_route();
-}
+  public:
+    URLmap pattern_to_callback;
+    std::vector<std::string> patterns_list;
+    Router(URLmap routes) {
+      for (std::pair<std::string, functor> p : routes) {
+        add_route(p.first, p.second);
+      }
+      // "calls add_route to populate pattern_to_callback and patterns_list"
+    };
+    void add_route(std::string url_pattern, functor f);
+    // Note: register() should be renamed from add_route() for django mimicking,
+    // but "register" is a reserved word in C++.
+    std::string resolve(HttpRequest);
+    HttpResponse get_http_response(HttpRequest);
+};
