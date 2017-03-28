9782831ad3106e4b43c44398ed013431e2d86092
official71 <yqi71@hotmail.com>
2017-03-28 17:44:53 -0400

    prototype of app: new thread for each request

---
+++ app/app.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "app.hpp"
+
+using namespace std;
+
+#define BUFFER_MAXSIZE 1024
+
+void App::print_routes()
+{
+    cout << "Hello app!" << endl;
+}
+
+void error_exit(string msg)
+{
+    // _DEBUG(msg);
+    perror(msg.c_str());
+    exit(1);
+}
+
+static void worker(int clntSock)
+{
+    char buff[BUFFER_MAXSIZE];
+    memset(buff, 0, BUFFER_MAXSIZE);
+    int len = read(clntSock, buff, BUFFER_MAXSIZE-1);
+    if (len < 0) {
+        _DEBUG("Failed to read from client");
+        close(clntSock);
+        return;
+    }
+    // _DEBUG("Received request from client:\n", string(buff));
+
+    HttpRequest request(buff);
+    HttpResponse response = get_phony_response(request);
+
+    string resp = "HTTP/1.1 200 OK\r\n"
+                  "Content-Type: text/html\r\n\r\n";
+    resp += response.content + "\r\n\r\n";
+    // _DEBUG("Response:\n", resp);
+    // if (write(clntSock, resp.c_str(), resp.length()) < 0)
+    if (::send(clntSock, resp.c_str(), resp.length(), 0) < 0)
+        _DEBUG("Failed to send response on socket: ", clntSock);
+    close(clntSock);
+}
+
+void App::handle_request(int clntSock)
+{
+    thread t(worker, clntSock);
+    auto id = t.get_id();
+    t.detach();
+    _DEBUG("Created and detached new thread ", id, " for socket ", clntSock);
+}
+
+void App::run(int port)
+{
+    _DEBUG("Invoked for port: ", port);
+
+    int servSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (servSock < 0)
+        error_exit("Failed to create server socket");
+
+    struct sockaddr_in servAddr;
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAddr.sin_port = htons(port);
+    if (::bind(servSock, (struct sockaddr*)&servAddr, sizeof(servAddr)) < 0)
+        error_exit("Failed to bind server address");
+
+    if (listen(servSock, 5) < 0)
+        error_exit("Failed to listen");
+
+    for (;;) {
+        struct sockaddr_in clntAddr;
+        unsigned int clntLen;
+        int clntSock = ::accept(servSock, (struct sockaddr*)&clntAddr, &clntLen);
+        if (clntSock < 0)
+            error_exit("Failed to accept");
+
+        _DEBUG("Call handling request for socket: ", clntSock);
+        this->handle_request(clntSock);
+    }
+}
---
+++ app/externs.hpp
@@ -0,0 +1,62 @@
+#ifndef _EXTERNS_HPP
+#define _EXTERNS_HPP
+
+#include <iostream>
+#include <map>
+#include <functional>
+#include <string>
+
+using std::string;
+
+/* print debug messages if -DDEBUG specified */
+#ifdef DEBUG
+inline void _log() {}
+template<typename Head, typename ...Rest>
+inline void _log(Head && h, Rest && ...r)
+{
+    std::cout << std::forward<Head>(h);
+    _log(std::forward<Rest>(r)...);
+}
+#define _DEBUG(...) do\
+{\
+    std::cout << "[ " << __FILE__;\
+    std::cout << " -> " << __FUNCTION__;\
+    std::cout << " -> " << __LINE__ << " ] ";\
+    _log(__VA_ARGS__);\
+    std::cout << endl;\
+} while(0)
+#else
+#define _DEBUG(...) do{} while(0)
+#endif
+
+class HttpRequest {
+public:
+    string data;
+    HttpRequest(string str) : data(str) {}
+};
+
+class HttpResponse {
+public:
+    string content;
+    HttpResponse(string str) : content(str) {}
+};
+
+inline HttpResponse get_phony_response(HttpRequest req)
+{
+    string text = "<html>\r\n<body>\r\n"
+                 "<h1>Hi there!</h1>\r\n"
+                 "<p>This is just a phony response :P</p>\r\n"
+                 "</body>\r\n</html>";
+    HttpResponse resp(text);
+    return resp;
+}
+
+typedef std::function<HttpResponse(HttpRequest)> functor;
+typedef std::map<std::string, functor> URLmap;
+
+class Router {
+public:
+    URLmap pattern_to_callback;
+};
+
+#endif
