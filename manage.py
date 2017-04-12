56a47059033bb111b5f5c57271cb324418b0afc3
caprice-j <tnk.yasutaka@gmail.com>
2017-04-12 10:49:01 -0400

    pass port_number to manage.py

--- manage.py
+++ manage.py
@@ -3,4 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-  os.system('./runapp runserver 8000')
+  if len(sys.argv) > 1 and sys.argv[1] == 'runserver':
+    port_number = sys.argv[2] if len(sys.argv) > 2 else '8000'
+    os.system('./runapp runserver ' + port_number)
+  else:
+    print("usage: python manage.py runserver <port number (default:8000)>")
