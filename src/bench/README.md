3a7e0ae09332f93cd268e6bf0cf188955e2dbd86
caprice-j <tnk.yasutaka@gmail.com>
2017-04-19 02:11:13 -0400

    add performance result

---
+++ src/bench/README.md
@@ -0,0 +1,35 @@
+# Results
+
+![](performance.png)
+
+# Overview
+
++ Executed the following commands from a 1.2GHz single board computer (Raspberry Pi 3)
+  + `nc=${num}; name=cjango-c${nc}-1; ab -n 10000 -c ${nc} -g ${name}.log http://192.168.0.15:8000/ > ${name}.txt`
+  + `${num}` is set as 1, 3, 5, 7, 10, 50, 100, 150
+    + Django cannot handle >=15 concurrent requests (`connection reset by peer` error)
+    + All C++ libraries cannot handle 200 concurrent requests (the same as above)
+  + all raw log files are stored under `bench/ethernet/`
+
+# Experimental Conditions
+
+#### Server Spec
+
+  + Macbook Pro Mid 2014
+    + OS X 10.11.6 (El Capitan)
+    + 3GHz Intel Core i7 Dual Core
+    + 16GB DDR3 memory
+
+#### Client Spec
+
+  + [Raspberry Pi 3 Model B+](https://www.raspberrypi.org/products/raspberry-pi-3-model-b/)
+
+#### Network settings
+  + (Raspberry Pi 3) <- 100Mbps ethernet (RPI3's maximum) -> Router <- 1Gbps Ether-Thunderbolt adapter -> Macbook
+
+#### App Framework Settings
+
+  + remove all URL mappings except one (root dir `/` -> a function to print out `"HelloWorld"`)
+    + silicon/django uses `/hello/` as URL
+  + disable all debugging messages for all app frameworks
+  + use the same port (8000)
---
+++ src/bench/show_ab_result.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+library(stringr)
+f <- 'cjango-c1-1.log'
+out <- NULL
+for (f in dir("./", pattern = ".log")) {
+    app_name <- gsub("-.*","", f)
+    n_concurrent <- gsub(".*-c([0-9]*)-.*", "\\1", f, perl = TRUE)
+    take <- gsub(".*-(.*)\\.log", "\\1", f, perl = TRUE)
+    data.frame()
+    
+    app_name <- ifelse(app_name=="cjango", "Cjango (C++)",
+                       ifelse(app_name=='crow', "Crow (C++, Github Star #1)",
+                              ifelse(app_name=='django',"Django (Python)", "Silicon (C++, Star #2)")))
+    
+    a_row <- bind_cols(data.frame(  app = rep(app_name,10000),
+                                   ncon = rep(n_concurrent, 10000) %>% as.integer,
+                                   take = rep(take, 10000)),
+                        read.delim(f, stringsAsFactors = FALSE))
+    if (is.null(out))
+        out <- a_row
+    else
+        out <- bind_rows(out, a_row)
+    message(f)
+}
+
+out <- out %>% mutate( less15 = ifelse(ncon < 300, "less", "not") %>% as.factor())
+
+res <- out %>% group_by(app, ncon) %>% filter(take==1 & (! ncon == 11) ) %>% summarise( ttime = mean(ttime) )
+
+library(ggforce)
+library(ggbash)
+ggplot(as.data.frame(res), aes(x=ncon, y=ttime, color=app, group=app)) + geom_line(size=1.5) + geom_point(size=9)+ geom_point(size=7, color="white") +
+    geom_text(aes(label=substr(app,1,2)), fontface="bold") + facet_zoom(xlim=c(1,10.1), ylim=c(0,14)) + theme(legend.position = c(.2,.7)) + theme2(l.txt(sz=15, f="bold"), a.txt(size=18, f="bold"), a.ttl(f="bold", sz=20)) + labs(x="Number of Concurrent Connections", y="Total Time of Apache Bench [ms/req]")
+
+
+ggplot(res, aes(x=as.character(ncon) %>% str_pad(pad="0", width=3), y=ttime, fill=reorder(app, -ttime) )) + geom_bar(stat="identity", position="dodge") 
