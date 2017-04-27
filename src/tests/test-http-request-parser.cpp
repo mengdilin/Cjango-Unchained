6f9e389937f6fc50d9852c9f204d673b0af605e1
Mengdi Lin <mengdilin95@gmail.com>
2017-04-27 19:25:33 +0000

    add more unit tests for http-request-parser

--- src/tests/test-http-request-parser.cpp
+++ src/tests/test-http-request-parser.cpp
@@ -4,6 +4,41 @@
 #include <sstream>
 #include <unordered_map>
 
+TEST_CASE("HttpRequestParser - request scheme parsing", "[HttpRequestParser]") {
+  std::string get_test = "GET /path/file.html HTTP/1.0\r\nUser-Agent:HTTPTool/1.0\r\n\r\n";
+  http::HttpRequestParser parser;
+  std::istringstream ss(get_test);
+  http::HttpRequest headers = parser.parse_request_line_and_headers(ss);
+  REQUIRE( headers.get_scheme() == "HTTP/1.0" );
+}
+
+TEST_CASE("HttpRequestParser - request method parsing", "[HttpRequestParser]") {
+  std::string get_test = "GET /path/file.html HTTP/1.0\r\nUser-Agent:HTTPTool/1.0\r\n\r\n";
+  http::HttpRequestParser parser;
+  std::istringstream ss(get_test);
+  http::HttpRequest headers = parser.parse_request_line_and_headers(ss);
+  REQUIRE( headers.get_method() == "GET" );
+}
+
+TEST_CASE("HttpRequestParser - request path parsing", "[HttpRequestParser]") {
+  std::string get_test = "GET /path/file.html HTTP/1.0\r\nUser-Agent:HTTPTool/1.0\r\n\r\n";
+  http::HttpRequestParser parser;
+  std::istringstream ss(get_test);
+  http::HttpRequest headers = parser.parse_request_line_and_headers(ss);
+  REQUIRE( headers.get_path() == "/path/file.html" );
+}
+
+TEST_CASE("HttpRequestParser - request headers param parsing", "[HttpRequestParser]") {
+  std::string get_test = "GET /path/file.html HTTP/1.0\r\nUser-Agent:HTTPTool/1.0\r\n\r\n";
+  http::HttpRequestParser parser;
+  std::istringstream ss(get_test);
+  http::HttpRequest headers = parser.parse_request_line_and_headers(ss);
+  auto result = headers.get_meta().find("User-Agent");
+  REQUIRE( result != headers.get_meta().end() );
+  REQUIRE( result->second == "HTTPTool/1.0" );
+}
+
+
 TEST_CASE("HttpRequestParser - request parsing", "[HttpRequestParser]") {
   std::string get_test = "GET /path/file.html HTTP/1.0\r\nUser-Agent:HTTPTool/1.0\r\n\r\n";
   http::HttpRequestParser parser;
@@ -12,7 +47,6 @@ TEST_CASE("HttpRequestParser - request parsing", "[HttpRequestParser]") {
 
   REQUIRE( headers.get_method() == "GET" );
   REQUIRE( headers.get_path() == "/path/file.html" );
-  REQUIRE( headers.get_scheme() == "HTTP/1.0" );
   REQUIRE( headers.get_scheme() == "HTTP/1.0" );
   auto result = headers.get_meta().find("User-Agent");
   REQUIRE( result != headers.get_meta().end() );
@@ -36,6 +70,26 @@ TEST_CASE("HttpRequestParser - get param request parsing", "[HttpRequestParser]"
   auto param_result = params.find("abc");
   REQUIRE( param_result != params.end() );
   REQUIRE( param_result->second == "aa" );
+}
+
+TEST_CASE("HttpRequestParser - post request cookie parsing", "[HttpRequestParser]") {
+  std::string get_test = "POST /cgi-bin/process.cgi HTTP/1.1\r\nContent-Type: text/xml; charset=utf-8; application/x-www-form-urlencoded\r\nContent-Length: 100\r\nCookie: keyofcookie=valueofcookie;\r\n\r\n";
+
+  http::HttpRequestParser parser;
+  std::istringstream ss(get_test);
+  http::HttpRequest headers = parser.parse(ss);
+
+  REQUIRE( headers.get_method() == "POST" );
+  REQUIRE( headers.get_path() == "/cgi-bin/process.cgi" );
+  REQUIRE( headers.get_scheme() == "HTTP/1.1" );
+  auto result = headers.get_meta().find("Content-Type");
+  REQUIRE( result != headers.get_meta().end() );
+  REQUIRE( result->second == " text/xml; charset=utf-8; application/x-www-form-urlencoded" );
+  auto cookie = headers.get_cookie();
+  auto cookie_result = cookie.find("keyofcookie");
+  REQUIRE( cookie_result != cookie.end() );
+  REQUIRE( cookie_result->second == "valueofcookie");
+
 }
 
 TEST_CASE("HttpRequestParser - post param request parsing", "[HttpRequestParser]") {
@@ -64,3 +118,32 @@ TEST_CASE("HttpRequestParser - post param request parsing", "[HttpRequestParser]
   REQUIRE( param_result != params.end() );
   REQUIRE( param_result->second == "string" );
 }
+TEST_CASE("HttpRequestParser - post param with special characters request parsing", "[HttpRequestParser]") {
+  std::string get_test = "POST /cgi-bin/process.cgi HTTP/1.1\r\nContent-Type: text/xml; charset=utf-8; application/x-www-form-urlencoded\r\nContent-Length: 100\r\n\r\n"
+  "licenseID=string&content=string&/paramsXML=string&special=%F0%9F%91%8D\r\n";
+
+  http::HttpRequestParser parser;
+  std::istringstream ss(get_test);
+  http::HttpRequest headers = parser.parse(ss);
+
+  REQUIRE( headers.get_method() == "POST" );
+  REQUIRE( headers.get_path() == "/cgi-bin/process.cgi" );
+  REQUIRE( headers.get_scheme() == "HTTP/1.1" );
+  auto result = headers.get_meta().find("Content-Type");
+  REQUIRE( result != headers.get_meta().end() );
+  REQUIRE( result->second == " text/xml; charset=utf-8; application/x-www-form-urlencoded" );
+  auto params = headers.get_parameters();
+  REQUIRE(params.size() != 0);
+  auto param_result = params.find("licenseID");
+  REQUIRE( param_result != params.end() );
+  REQUIRE( param_result->second == "string" );
+  param_result = params.find("content");
+  REQUIRE( param_result != params.end() );
+  REQUIRE( param_result->second == "string" );
+  param_result = params.find("/paramsXML");
+  REQUIRE( param_result != params.end() );
+  REQUIRE( param_result->second == "string" );
+  param_result = params.find("special");
+  std::cout << param_result->second << std::endl;
+}
+
