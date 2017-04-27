1e8b6c079908d383066c39787ae5214d39dde381
Mengdi Lin <mengdilin95@gmail.com>
2017-04-27 19:33:40 +0000

    add more http-request-parser for cookie

--- src/tests/test-http-request-parser.cpp
+++ src/tests/test-http-request-parser.cpp
@@ -147,3 +147,28 @@ TEST_CASE("HttpRequestParser - post param with special characters request parsin
   std::cout << param_result->second << std::endl;
 }
 
+TEST_CASE("HttpRequestParser - post request nasty cookie parsing", "[HttpRequestParser]") {
+  std::string get_test = "POST /cgi-bin/process.cgi HTTP/1.1\r\nContent-Type: text/xml; charset=utf-8; application/x-www-form-urlencoded\r\nContent-Length: 100\r\nCookie: keyofcookie=valueofcookie; username-localhost-8888=\"2|1:0|10:1492550910|23:username-localhost-8888|44:MWE1ZGRIODljODk3NGI0YmJIYjl=|02211\"; session=10596601668567;\r\n\r\n";
+
+  http::HttpRequestParser parser;
+  std::istringstream ss(get_test);
+  http::HttpRequest headers = parser.parse(ss);
+
+  REQUIRE( headers.get_method() == "POST" );
+  REQUIRE( headers.get_path() == "/cgi-bin/process.cgi" );
+  REQUIRE( headers.get_scheme() == "HTTP/1.1" );
+  auto result = headers.get_meta().find("Content-Type");
+  REQUIRE( result != headers.get_meta().end() );
+  REQUIRE( result->second == " text/xml; charset=utf-8; application/x-www-form-urlencoded" );
+  auto cookie = headers.get_cookie();
+  auto cookie_result = cookie.find("keyofcookie");
+  REQUIRE( cookie_result != cookie.end() );
+  REQUIRE( cookie_result->second == "valueofcookie");
+  cookie_result = cookie.find("username-localhost-8888");
+  REQUIRE( cookie_result != cookie.end() );
+  REQUIRE( cookie_result->second == "\"2|1:0|10:1492550910|23:username-localhost-8888|44:MWE1ZGRIODljODk3NGI0YmJIYjl=|02211\"");
+  cookie_result = cookie.find("session");
+  REQUIRE( cookie_result->second == "10596601668567");
+}
+
+
